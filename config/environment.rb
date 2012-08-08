3c7c78e07d1a6b510f684bbfb97f4eb77c48e6e4
Chulakorn Aritajati <chulakornaritajati@gmail.com>
2012-08-08 00:01:07 -0500

    New generate files
    $ rails new myapp -m https://github.com/RailsApps/rails3-application-templates/raw/master/rails3-bootstrap-devise-cancan-template.rb -T

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
-GreenDolphins::Application.initialize!
+GreenDophins::Application.initialize!
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-GreenDolphins::Application.config.secret_token = 'd2d8d9bcbcb7d6f89ba3d0b2715350deec9edc61def8cf64df362232d51a79520f6fc6b74bdb053f4a8bac52e157c5ef7d88a6e9cf8cc06a51a2066abf63139f'
+GreenDophins::Application.config.secret_token = 'c10916090dc2a8610174113aea4f3e4376496b7df4ab509f2c1b207353ae33b9a2824cdf2e94fffc5c4000b9251fa9d098a017e3c5d03baaec8a51ff9ceac000'
