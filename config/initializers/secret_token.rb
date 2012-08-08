6cef81120f3ef659f676cbf2cfaab88176e1fa90
Chulakorn Aritajati <chulakornaritajati@gmail.com>
2012-08-07 22:12:34 -0500

    Initial Green Dolphin
    All files are original.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GreenDolphins::Application.config.secret_token = 'd2d8d9bcbcb7d6f89ba3d0b2715350deec9edc61def8cf64df362232d51a79520f6fc6b74bdb053f4a8bac52e157c5ef7d88a6e9cf8cc06a51a2066abf63139f'
