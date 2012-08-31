bd8ed92af220b790c7901a4c1cfcd3035af28b56
Chulakorn Aritajati <chulakornaritajati@gmail.com>
2012-08-31 16:07:32 -0500

    Rspec Devise
    Add a devise log in in a Rspec controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe UsersController do
 
   before (:each) do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
     @user = FactoryGirl.create(:user)
     sign_in @user
   end
