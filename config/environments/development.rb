57e518ea96575620ee31b53a1f4e4b60a5424b95
Chulakorn Aritajati <chulakornaritajati@gmail.com>
2012-09-01 22:46:56 -0500

    Add no.reply.auburn@gmail.com

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,11 +24,11 @@ GreenDophins::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "example.com",
+    domain: "auburn.edu",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: "no.reply.auburn@gmail.com",
+    password: "GoS1GCH1"
   }
 
 
