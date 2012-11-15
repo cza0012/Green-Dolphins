582a639c0e066d388602b1184875cea7a20c93c3
Chulakorn Aritajati <chulakornaritajati@gmail.com>
2012-11-15 00:46:46 -0600

    Set links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 GreenDophins::Application.routes.draw do
   #fix it later
-  get 'tags/:tag', to: 'questions#index', as: :tag
+  get 'tags/:tag', to: 'questions#index', as: :tags
+  # resources :tags, only: [:show, :index]
   
   resources :notifications
 
