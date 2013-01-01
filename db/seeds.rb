f54d185624c7a8dba2baf590894c3f1db6c2cb39
Chulakorn Aritajati <chulakornaritajati@gmail.com>
2013-01-01 14:36:04 -0600

    Data seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please', :confirmed_at => Time.now.utc
+user = User.create! :name => 'Chulakorn Aritajati', :email => 'cza0012@auburn.edu', :password => '3141085', :password_confirmation => '3141085', :confirmed_at => Time.now.utc
 puts 'New user created: ' << user.name
-user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please', :confirmed_at => Time.now.utc
+user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => '3141085', :password_confirmation => 'please', :confirmed_at => Time.now.utc
 puts 'New user created: ' << user2.name
 user.add_role :admin
+user.add_role :expert
+user2.add_role :expert
+user.add_role :instructor
