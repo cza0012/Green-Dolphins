9f45f7250f689ea5c84468313dc845452829ee97
Chulakorn Aritajati <chulakornaritajati@gmail.com>
2013-01-01 16:02:02 -0600

    .to_i

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 puts 'SETTING UP DEFAULT USER LOGIN'
 user = User.create! :name => 'Chulakorn Aritajati', :email => 'cza0012@auburn.edu', :password => '3141085', :password_confirmation => '3141085', :confirmed_at => Time.now.utc
 puts 'New user created: ' << user.name
-user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => '3141085', :password_confirmation => 'please', :confirmed_at => Time.now.utc
+user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => '3141085', :password_confirmation => '3141085', :confirmed_at => Time.now.utc
 puts 'New user created: ' << user2.name
 user.add_role :admin
 user.add_role :expert
